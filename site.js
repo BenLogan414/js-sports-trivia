6382a81bb91f8d77c3d21057b710e610f428121a
Logan <500202940@fvtc.edu>
2024-10-21 13:56:01 -0500

    Completed Exam 2

--- site.js
+++ site.js
@@ -8,13 +8,46 @@ const answersElement = document.querySelector('#answers')
 const nextQuestionElement = document.querySelector('#nextQuestion')
 
 // IIFE (so we can use async/await)
-(async () => {
+;(async () => {
 
 	// todo: create your "getNextQuestion" function
+	const getNextQuestion = async () => {
+		const url = 'https://opentdb.com/api.php?amount=1&category=21&difficulty=easy&type=multiple'
+		const response = await fetch(url)
+		const json = await response.json()
+		const { question, correct_answer: correct, incorrect_answers: incorrect } = json.results[0]
+		const answers = shuffle([ ...incorrect, correct ])
+		return { question, answers, correct }
+	}
 
 	// todo: create your "renderQuestion" function
-
+	const renderQuestion = ({ question, answers, correct }) => {
+		questionElement.innerHTML = ''
+		answersElement.innerHTML = ''
+		questionElement.textContent = decodeHtml(question)
+		answers.forEach(answer => {
+			const button = document.createElement('button')
+			button.textContent = decodeHtml(answer)
+			button.addEventListener('click', async () => {
+				if (answer === correct) {
+					button.classList.add('correct')
+					answersElement.querySelectorAll('button').forEach(b => b.disabled = true)
+					alert('Correct!')
+					return
+				}
+				
+				button.disabled = true
+				alert('Incorrect!')
+			})
+			answersElement.append(button)
+		})
+	}
 	// todo: add the event listener to the "nextQuestion" button
+	nextQuestionElement.addEventListener('click', async () => {
+		renderQuestion(await getNextQuestion())
+		nextQuestionElement.disabled = true
+		setTimeout(() => nextQuestionElement.disabled = false, 10000)
+	})
 
 })()
 
